89d8260ee35c94e389ae564b8f450d76419ad303
2d6 <somethingcorporate-github@2d6.de>
2015-05-01 13:21:40 +0200

    Blank strings now again allowed as State identifiers

--- src/main/java/automata/DeterministicFiniteAutomaton.java
+++ src/main/java/automata/DeterministicFiniteAutomaton.java
@@ -30,8 +30,7 @@ public interface DeterministicFiniteAutomaton<T> {
 	 * identifier, an IllegalArgumentException is thrown.
 	 * 
 	 * @param identifier
-	 *            The identifier of the new state. Must not be blank (e.g.
-	 *            whitespaces) or empty.
+	 *            The identifier of the new state.
 	 * @param isAccepting
 	 *            The acceptance status of the new state. True if the state is
 	 *            accepting.
