199917da7a27f16d67b956f6dd2d8463253bd0f7
Moritz Wagner <moritz.wagner@qudosoft.de>
2015-05-20 15:11:56 +0200

    Moved the structural comparison methods to a private class

--- src/main/java/automata/DeterministicFiniteAutomaton.java
+++ src/main/java/automata/DeterministicFiniteAutomaton.java
@@ -20,9 +20,6 @@ public class DeterministicFiniteAutomaton<T> implements IDeterministicFiniteAuto
 	protected HashMap<String, State> states;
 	protected State startingState;
 	protected ITransitionFunction<T> transitionFunction;
-	
-	protected static final boolean NOT_ACCEPTING = false;
-	protected static final State ILLEGAL_SYMBOL_STATE = new State("Illegal symbol occurred", NOT_ACCEPTING);
 
 	/**
 	 * Creates a new automaton with a starting state.
@@ -152,52 +149,7 @@ public class DeterministicFiniteAutomaton<T> implements IDeterministicFiniteAuto
 
 	@Override
 	public boolean isStructurallyEqualTo(IDeterministicFiniteAutomaton<T> otherCharDfa) {
-		List<State> visitedStates = new ArrayList<>();
-		List<State> visitedStatesOther = new ArrayList<>();
-		return stateSubGraphIsEqual(this, getStartingState(),
-				otherCharDfa, otherCharDfa.getStartingState(), visitedStates,
-				visitedStatesOther);
-	}
-
-	private boolean stateSubGraphIsEqual(IDeterministicFiniteAutomaton<T> dfaA, State currentStateA,
-			IDeterministicFiniteAutomaton<T> dfaB, State currentStateB, List<State> visitedStatesA,
-			List<State> visitedStatesB) {
-
-		if (currentStateA.isAccepting() != currentStateB.isAccepting()) {
-			return false;
-		} 
-		else if (visitedStatesA.contains(currentStateA) || visitedStatesB.contains(currentStateB)) {
-			return visitedStatesA.indexOf(currentStateA) == visitedStatesB.indexOf(currentStateB);
-		}
-
-		/* 
-		 * If the states have already been visited, check whether the are
-		 * equal by their position in the visit history
-		 */
-
-		visitedStatesA.add(currentStateA);
-		visitedStatesB.add(currentStateB);
-
-		Set<T> validSymbolsA = dfaA.getValidSymbols(currentStateA);
-		Set<T> validSymbolsB = dfaB.getValidSymbols(currentStateB);
-
-		if (validSymbolsA.equals(validSymbolsB)) {
-
-			/*
-			 * Recursively check all subgraphs of the current State
-			 */
-
-			for (T symbol : validSymbolsA) {
-				State nextStateA = dfaA.getNextState(currentStateA, symbol);
-				State nextStateB = dfaB.getNextState(currentStateB, symbol);
-				if (!stateSubGraphIsEqual(dfaA, nextStateA, dfaB,
-								nextStateB, visitedStatesA, visitedStatesB)) {
-					return false;  
-				}
-			}
-			return true;
-		}
-		return false;
+		return new DfaStructureComparator().structurallyEqual(this, otherCharDfa);
 	}
 	
 	@Override
@@ -208,6 +160,67 @@ public class DeterministicFiniteAutomaton<T> implements IDeterministicFiniteAuto
 	@Override
 	public Set<T> getValidSymbols(State currentState) {
 		return this.transitionFunction.getValidSymbols(currentState);
+	}
+	
+	private class DfaStructureComparator {
+		private IDeterministicFiniteAutomaton<T> dfaA;
+		private List<State> visitedStatesA;
+		
+		private IDeterministicFiniteAutomaton<T> dfaB;
+		private List<State> visitedStatesB;
+		
+		public boolean structurallyEqual(
+				IDeterministicFiniteAutomaton<T> dfaA, 
+				IDeterministicFiniteAutomaton<T> dfaB) {
+			
+			this.dfaA = dfaA;
+			this.visitedStatesA = new ArrayList<>();
+
+			this.dfaB = dfaB;
+			this.visitedStatesB = new ArrayList<>();
+			
+			return this.stateSubGraphIsEqual(dfaA.getStartingState(), dfaB.getStartingState());
+		}
+		
+		private boolean stateSubGraphIsEqual(State currentStateA, State currentStateB) {
+
+			if (currentStateA.isAccepting() != currentStateB.isAccepting()) {
+				return false;
+			} 
+			else if (visitedStatesA.contains(currentStateA) || visitedStatesB.contains(currentStateB)) {
+				return visitedStatesA.indexOf(currentStateA) == visitedStatesB.indexOf(currentStateB);
+			}
+
+			/* 
+			 * If the states have already been visited, check whether the are
+			 * equal by their position in the visit history
+			 */
+
+			visitedStatesA.add(currentStateA);
+			visitedStatesB.add(currentStateB);
+
+			Set<T> validSymbolsA = dfaA.getValidSymbols(currentStateA);
+			Set<T> validSymbolsB = dfaB.getValidSymbols(currentStateB);
+
+			if (validSymbolsA.equals(validSymbolsB)) {
+
+				/*
+				 * Recursively check all subgraphs of the current State; simply returns true if the 
+				 * State has no valid Symbols (i.e. transitions) 
+				 */
+
+				for (T symbol : validSymbolsA) {
+					State nextStateA = dfaA.getNextState(currentStateA, symbol);
+					State nextStateB = dfaB.getNextState(currentStateB, symbol);
+					if (!stateSubGraphIsEqual(nextStateA, nextStateB)) {
+						return false;  
+					}
+				}
+				return true;
+			}
+			return false;
+		}
+		
 	}
 	
 }
